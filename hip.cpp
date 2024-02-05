252eaa90046e10cb18eb54b63ec71854f8065392
michbogos <bogostamicheal@gmail.com>
2024-02-05 19:56:06 +0200

    Coloring for hip

--- hip.cpp
+++ hip.cpp
@@ -18,23 +18,28 @@
 
 using namespace std;
 
-__global__ void MyKernel(unsigned char*frame, int N)
+
+__device__ float interpolate(float a, float b, float t){
+	return a*(1.0f-t)+b*t;
+}
+
+__global__ void MyKernel(unsigned char*frame, float scale, int N)
 {
-	hipComplex c = make_hipComplex(blockIdx.x/256.0f-1.0f-0.0700432019411218f, blockIdx.y/256.0f-1.0f-0.8224676332988761f);
-	hipComplex i = make_hipComplex(blockIdx.x/256.0f-1.0f-0.0700432019411218f, blockIdx.y/256.0f-1.0f-0.8224676332988761f);
+	float colors[6][3] = {{255, 156, 87},{87, 165, 187},{198, 98, 123},{76, 71, 237},{143, 97, 198},{198, 34, 243}};
+	hipDoubleComplex c = make_hipDoubleComplex((blockIdx.x/256.0f-1.0f)*scale-0.0700432019411218, (blockIdx.y/256.0f-1.0f)*scale-0.8224676332988761);
+	hipDoubleComplex i = make_hipDoubleComplex((blockIdx.x/256.0f-1.0f)*scale-0.0700432019411218, (blockIdx.y/256.0f-1.0f)*scale-0.8224676332988761);
 	for(int n = 0; n < N; n++){
 		i = hipCadd(hipCmul(i, i), c);
+		if(hipCabs(i)>10e10f){
+			frame[(512*blockIdx.x+blockIdx.y)*3+0] = (int)exp(sqrt(sqrt(n)));
+			frame[(512*blockIdx.x+blockIdx.y)*3+1] = (int)exp(sqrt(sqrt(n)));
+			frame[(512*blockIdx.x+blockIdx.y)*3+2] = (int)exp(sqrt(sqrt(n)));
+			return;
+		}
 	}
-	if(hipCabs(i)<100.0f){
-		frame[(512*blockIdx.x+blockIdx.y)*3+0] = 255;
-		frame[(512*blockIdx.x+blockIdx.y)*3+1] = 255;
-		frame[(512*blockIdx.x+blockIdx.y)*3+2] = 255;
-	}
-	else{
-		frame[(512*blockIdx.x+blockIdx.y)*3+0] = 0;
-		frame[(512*blockIdx.x+blockIdx.y)*3+1] = 0;
-		frame[(512*blockIdx.x+blockIdx.y)*3+2] = 0;
-	}
+	frame[(512*blockIdx.x+blockIdx.y)*3+0] = 0;
+	frame[(512*blockIdx.x+blockIdx.y)*3+1] = 0;
+	frame[(512*blockIdx.x+blockIdx.y)*3+2] = 0;
 	return;
 }
 
@@ -47,14 +52,14 @@ int main(int argc, char* argv[])
     cout << " agent prop name " << devProp.name << endl;
 
 
-	unsigned char *output = (unsigned char*) malloc(512*512*4);
+	unsigned char *output = (unsigned char*) malloc(512*512*3);
 
 	unsigned char* outputBuffer;
-    hipMalloc((void**)&outputBuffer, 512*512*4*sizeof(unsigned char));
+    hipMalloc((void**)&outputBuffer, 512*512*3*sizeof(unsigned char));
 
-	MyKernel<<<dim3(512, 512), dim3(1, 1), 0, 0>>> (outputBuffer, 1000, );
+	MyKernel<<<dim3(512, 512), dim3(1, 1), 0, 0>>> (outputBuffer, 10e-14, 1000);
 
-	hipMemcpy(output, outputBuffer,512*512*4, hipMemcpyDeviceToHost);
+	hipMemcpy(output, outputBuffer,512*512*3, hipMemcpyDeviceToHost);
 	stbi_write_png("hip.png", 512, 512, 3, output, 512*3);
     hipFree(outputBuffer);
 	return SUCCESS;
