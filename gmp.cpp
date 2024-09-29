80906ca80f83ba4d5a74187dc7b8b672729295d0
michbogos <bogostamicheal@gmail.com>
2024-09-29 20:58:08 +0200

    Add gmp library

--- gmp.cpp
+++ gmp.cpp
@@ -21,28 +21,39 @@ unsigned int Mand (mpf_t initx, mpf_t inity, unsigned int iterations) {
     mpf_init(xsq);
     mpf_init(ysq);
     mpf_init(tmp);
-    unsigned int iter;
+    unsigned int iter = 0;
 
-    mpf_mul(tmp, initx, initx);
-    mpf_add(cx, cx,tmp);
-    mpf_add(cx, cx,initx);
-    mpf_clear(tmp);
-    mpf_mul(tmp, inity, inity);
-    mpf_sub(cx, cx, tmp);
+    while(true){
+        mpf_pow_ui(ysq, cy, 2);
+        mpf_pow_ui(xsq, cx, 2);
+        mpf_add(tmp, xsq, ysq);
+        if(iter > iterations || mpf_cmp_ui(tmp, 4)>0) break;
+        iter++;
+        mpf_mul(tmp, cx, cy);
+        mpf_mul_ui(tmp, tmp, 2);
+        mpf_add(cy, inity, tmp);
 
-    mpf_add(cy, cy, inity);
-    mpf_clear(tmp);
-    mpf_mul(tmp, initx, inity);
-    mpf_add(cy, cy, tmp);
-    mpf_add(cy, cy, tmp);
+        mpf_add(tmp, ysq, xsq);
+        mpf_sub(cx, initx, tmp);
+    }
+    return iter;
+
+
+    // mpf_mul(tmp, initx, initx);
+    // mpf_add(cx, cx,tmp);
+    // mpf_add(cx, cx,initx);
+    // mpf_clear(tmp);
+    // mpf_mul(tmp, inity, inity);
+    // mpf_sub(cx, cx, tmp);
+
+    // mpf_add(cy, cy, inity);
+    // mpf_clear(tmp);
+    // mpf_mul(tmp, initx, inity);
+    // mpf_add(cy, cy, tmp);
+    // mpf_add(cy, cy, tmp);
 
     // cx=initx+initx*initx - inity*inity;   
     // cy=inity+initx*inity+initx*inity; 
-
-    
-    
-    for (iter=0;iter<iterations && (ysq=cy*cy)+(xsq=cx*cx)<4;iter++,cy=inity+cx*cy+cx*cy,cx=initx-ysq+xsq) ;
-    return (iter);
 }
 
 unsigned char colors[4][4] = {{255, 0, 0, 255},{0, 255, 0, 255},{0, 0, 255},{123, 230, 90, 255}};
@@ -51,7 +62,7 @@ double dy = - 0.8224676131988761;
 
 std::queue<int> tasks;
 
-void imagen(char* mag){
+void imagen(){
     mpf_t scale, dx, dy, scale_mul, iw, jh, width, height, x, y;
     mpf_init_set_str(dx, "-0.0700212907411218", 10);
     mpf_init_set_str(dy, "-0.8224676131988761", 10);
@@ -64,7 +75,7 @@ void imagen(char* mag){
         mpf_init_set_d(scale, 1.0);
         mpf_init_set_d(scale_mul, 0.99);
         for(int i = 0; i < frame; i++){
-            mpf_mul(scale, scale_mul);
+            mpf_mul(scale, scale, scale_mul);
         }
             for(int i = 0; i< WIDTH; i++){
                 mpf_set_d(iw, (double)i/WIDTH);
@@ -91,25 +102,26 @@ void imagen(char* mag){
             char buf[255];
             sprintf(buf, "./frames/%d.png", frame);
             stbi_write_png(buf, WIDTH, HEIGHT, 4, data,WIDTH*4);
-            printf("Generated frame: %d @ scale: %lf\n", frame, scale);
+            printf("Generated frame: %d", frame);
         }
     return;
 }
 
 
 int main(){
-    mpf_set_default_prec(128);
+    mpf_set_default_prec(64);
     int frame = 0;
     for(int i = 0; i < FRAMES; i++){
         tasks.push(i);
     }
-    std::vector<std::thread> threads;
-    for(int i = 0; i < NUM_THREADS; i++){
-        threads.push_back(std::thread(imagen));
-        frame ++;
-    }
-    for(int i = 0 ; i < NUM_THREADS; i++){
-        threads[i].join();
-    }
+    // std::vector<std::thread> threads;
+    // for(int i = 0; i < NUM_THREADS; i++){
+    //     threads.push_back(std::thread(imagen));
+    //     frame ++;
+    // }
+    // for(int i = 0 ; i < NUM_THREADS; i++){
+    //     threads[i].join();
+    // }
+    imagen();
     return 0;
 }
