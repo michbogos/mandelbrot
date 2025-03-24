cd864dc9b3ef214cc2b25881dc55839df290cee2
michbogos <bogostamicheal@gmail.com>
2025-03-24 21:32:00 +0100

    Generating mandelbrot set

--- impl_vulkan/main.cpp
+++ impl_vulkan/main.cpp
@@ -8,6 +8,10 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
+#define WIDTH 1920
+#define HEIGHT 1080
+#define WARP_SZIE 32
+
 
 int
 main()
@@ -15,30 +19,19 @@ main()
     kp::Manager mgr;
 
    auto tensorOut =
-      mgr.tensor(512*512*3, 4, kp::Memory::DataTypes::eFloat);
+      mgr.tensor(WIDTH*HEIGHT*3, 4, kp::Memory::DataTypes::eFloat);
 
     const std::vector<std::shared_ptr<kp::Memory>> params = { tensorOut };
 
     const std::vector<uint32_t> shader = std::vector<uint32_t>(
       shader::SHADER_COMP_SPV.begin(), shader::SHADER_COMP_SPV.end());
-    std::shared_ptr<kp::Algorithm> algo = mgr.algorithm(params, shader, {512, 512, 1});
+    std::shared_ptr<kp::Algorithm> algo = mgr.algorithm(params, shader, {(WIDTH+WARP_SZIE-1)/WARP_SZIE, (HEIGHT+WARP_SZIE-1)/WARP_SZIE, 1}, {WIDTH, HEIGHT}, {WIDTH, HEIGHT});
 
     mgr.sequence()
       ->record<kp::OpSyncDevice>(params)
-      ->record<kp::OpAlgoDispatch>(algo)
+      ->record<kp::OpAlgoDispatch>(algo, std::vector<uint32_t>{WIDTH, HEIGHT})
       ->record<kp::OpSyncLocal>(params)
       ->eval();
 
-      stbi_write_hdr("output.hdr", 512, 512, 3, tensorOut->vector<float>().data());
-
-    // // prints "Output {  0  4  12  }"
-    // std::cout << "Output: {  ";
-    // for (const float& elem : tensorOut->vector()) {
-    //     std::cout << elem << "  ";
-    // }
-    // std::cout << "}" << std::endl;
-
-    // if (tensorOut->vector() != std::vector<float>{ 0, 4, 12 }) {
-    //     throw std::runtime_error("Result does not match");
-    // }
+      stbi_write_hdr("output.hdr", WIDTH, HEIGHT, 3, tensorOut->vector<float>().data());
 }
