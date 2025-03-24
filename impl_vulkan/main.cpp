7a0a42cf7588bbb56c4ddf3c5ebc979ae5946d08
michbogos <bogostamicheal@gmail.com>
2025-03-24 13:30:46 +0100

    Uv generation

--- impl_vulkan/main.cpp
+++ impl_vulkan/main.cpp
@@ -4,27 +4,24 @@
 #include <vector>
 
 #include <kompute/Kompute.hpp>
-#include <shader/my_shader.hpp>
+#include <shader.hpp>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
 
 int
 main()
 {
     kp::Manager mgr;
 
-    std::shared_ptr<kp::TensorT<float>> tensorInA =
-      mgr.tensor({ 2.0, 4.0, 6.0 });
-    std::shared_ptr<kp::TensorT<float>> tensorInB =
-      mgr.tensor({ 0.0, 1.0, 2.0 });
-    std::shared_ptr<kp::TensorT<float>> tensorOut =
-      mgr.tensor({ 0.0, 0.0, 0.0 });
+   auto tensorOut =
+      mgr.tensor(512*512*3, 4, kp::Memory::DataTypes::eFloat);
 
-    const std::vector<std::shared_ptr<kp::Memory>> params = { tensorInA,
-                                                              tensorInB,
-                                                              tensorOut };
+    const std::vector<std::shared_ptr<kp::Memory>> params = { tensorOut };
 
     const std::vector<uint32_t> shader = std::vector<uint32_t>(
-      shader::MY_SHADER_COMP_SPV.begin(), shader::MY_SHADER_COMP_SPV.end());
-    std::shared_ptr<kp::Algorithm> algo = mgr.algorithm(params, shader);
+      shader::SHADER_COMP_SPV.begin(), shader::SHADER_COMP_SPV.end());
+    std::shared_ptr<kp::Algorithm> algo = mgr.algorithm(params, shader, {512, 512, 1});
 
     mgr.sequence()
       ->record<kp::OpSyncDevice>(params)
@@ -32,14 +29,16 @@ main()
       ->record<kp::OpSyncLocal>(params)
       ->eval();
 
-    // prints "Output {  0  4  12  }"
-    std::cout << "Output: {  ";
-    for (const float& elem : tensorOut->vector()) {
-        std::cout << elem << "  ";
-    }
-    std::cout << "}" << std::endl;
+      stbi_write_hdr("output.hdr", 512, 512, 3, tensorOut->vector<float>().data());
 
-    if (tensorOut->vector() != std::vector<float>{ 0, 4, 12 }) {
-        throw std::runtime_error("Result does not match");
-    }
+    // // prints "Output {  0  4  12  }"
+    // std::cout << "Output: {  ";
+    // for (const float& elem : tensorOut->vector()) {
+    //     std::cout << elem << "  ";
+    // }
+    // std::cout << "}" << std::endl;
+
+    // if (tensorOut->vector() != std::vector<float>{ 0, 4, 12 }) {
+    //     throw std::runtime_error("Result does not match");
+    // }
 }
